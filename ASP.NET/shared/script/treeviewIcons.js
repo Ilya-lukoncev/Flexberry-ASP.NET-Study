c7cc63c29e5fe1df9c36e479bd4ce6356509b67f
ilukontsev <Ilya4541@yandex.ru>
2017-02-20 16:12:42 +0500

    Лабораторная №2

---
+++ ASP.NET/shared/script/treeviewIcons.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    setTreeViewIcons();
+});
+
+// Добавить иконки в дерево навигации.
+function setTreeViewIcons() {
+    var $trElem;
+    for (i = 0; i < 6; i++) {
+        $trElem = $("#TreeView1n6Nodes").find("table:eq(" + i + ")").find("tbody:first").find("tr:first").find("td:eq(1)");
+        $trElem.css("background-image", "url('../../App_Themes/Spring/Images/TreeView" + i + ".png')");
+        $trElem.css("background-position", "center");
+    }
+}
